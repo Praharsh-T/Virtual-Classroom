1561021560d02c689cb6d06cc80daae13c2b60b8
Praharsh-T <ppraharsht@gmail.com>
2024-01-22 00:18:59 +0530

    app js conflict resolve

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,17 @@
 import React from 'react'
-import {Router,Route} from "react-router-dom"
-import Dashboard from './Pages/Dashboard'
-import Login from './Pages/Login'
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./Pages/Dashboard";
+import Login from "./Pages/Login";
 
 const App = () => {
   return (
-    <Router>
-<Route path='/dashboard' Component={Dashboard}/>
-<Route path='/login' Component={Login}/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-<Dashboard/>
-<Login/>
-
-
-    </Router>
-  
-  )
-}
+export default App
