ef6ad4f97b2a2c491b833097256177c903276332
Praharsh-T <ppraharsht@gmail.com>
2024-01-22 00:03:10 +0530

    loginsetup

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,18 @@
-import React from 'react'
-import {Router,Route} from "react-router-dom"
-import Dashboard from './Pages/Dashboard'
-import Login from './Pages/Login'
+import React from "react";
+import { Router, Route } from "react-router-dom";
+import Dashboard from "./Pages/Dashboard";
+import Login from "./Pages/Login";
 
-
-const App = ()=> {
+const App = () => {
   return (
     <Router>
-<Route path='/dashboard' Component={Dashboard}/>
-<Route path='/login' Component={Login}/>
-
-<Dashboard/>
-<Login/>
-
-
+      <Route path="/dashboard" Component={Dashboard} />
+      <Route path="/login" e Component={Login} />      
+      
+              <Dashboard />
+      <Login />
     </Router>
-  
-  )
-}
+  );
+};
 
-export default App
+export default App;
