a2497ef4baa3b56a2177d7880acbdf793e27af03
PavanDNaik <pawannaik396@gmail.com>
2024-01-21 22:23:17 +0530

    pg boilerplate

---
+++ server/db/connect.js
@@ -0,0 +1,15 @@
+const { Client, Pool } = require("pg");
+const POSTGRESS_URL = process.env.POSTGRESS_CONNECTION_STRING;
+const client = new Client({
+  connectionString: POSTGRESS_URL,
+});
+
+const connectToPostgress = async () => {
+  try {
+    await client.connect();
+    console.log("CONNECTED TO DATABASE");
+  } catch (e) {
+    console.log("CLIENT CONNECTTION ERROR", e);
+  }
+};
+module.exports = connectToPostgress;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { client, pool } = require("pg");
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
+const connectToPostgress = require("./db/connect");
+connectToPostgress();
 app.get("/", (req, res) => {
   res.json("Hii");
 });
