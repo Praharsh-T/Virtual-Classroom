37997f13adb61433cd5e32095534dacbfc1dc21d
Praharsh-T <ppraharsht@gmail.com>
2024-03-09 00:26:56 +0530

    changed file icon component

--- client/src/Components/ClassroomBadge.jsx
+++ client/src/Components/ClassroomBadge.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function ClassroomBadge({ classDetails }) {
   return (
     <Link
-      to={`/class/${classDetails.classroomname}/${classDetails.classroomid}`}
+      to={`/class/${classDetails.classroomname}/${classDetails.classroomid}/`}
       className='inline-block w-1/3 md:w-1/4 p-6 bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg border border-gray-200 rounded-lg shadow-lg hover:shadow-2xl transition duration-300 transform hover:-translate-y-1 hover:scale-105 m-2'>
       <div className='text-center mb-4'>
         <h2 className='text-lg lg:text-xl font-bold tracking-tight text-gray-800'>
--- client/src/Components/class/FileIcon.jsx
+++ client/src/Components/class/FileIcon.jsx
@@ -1,78 +1,72 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import ViewFile from "./ViewFile";
 
 function FileIcon({ file }) {
   const { classroomName, classroomid } = useParams();
   const [viewFile, setViewFile] = useState(false);
+
   function closeView() {
     setViewFile(false);
   }
+
   return (
     <div
       onClick={() => setViewFile(true)}
-      class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mb-5"
-    >
-      <img
-        class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
-        src="/docs/images/blog/image-4.jpg"
-        alt="some img"
-      />
-      <div class="flex flex-col justify-between p-4 leading-normal">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          {file.filename}
-        </h5>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          {file.filedescription}
-        </p>
-        <h4>{file.filetype}</h4>
+      className='inline-block mb-4 mr-4 max-w-sm w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5'>
+      <div className='bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'>
+        <img
+          className='object-cover w-full h-48 rounded-t-lg'
+          src='/docs/images/blog/image-4.jpg'
+          alt='File Icon'
+        />
+        <div className='p-4'>
+          <h5 className='text-lg font-bold text-gray-800'>{file.filename}</h5>
+          <p className='mt-2 text-sm text-gray-600 truncate'>
+            {file.filedescription}
+          </p>
+          <h4 className='mt-2 text-xs text-gray-500'>{file.filetype}</h4>
+        </div>
       </div>
-      <div
-        className={
-          viewFile === true
-            ? "overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full h-screen md:inset-0 bg-transparent backdrop-blur-sm"
-            : "hidden"
-        }
-      >
-        <div class="relative p-4 pt-2 w-full h-full">
-          <div class="relative rounded-lg shadow w-full h-full">
-            <div class="flex items-center p-4 md:p-5 border border-gray-200 rounded-lg dark:border-gray-600 w-full h-full">
-              <button
-                onClick={(e) => {
-                  e.stopPropagation();
-                  closeView();
-                  console.log(setViewFile);
-                }}
-                class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-              >
-                <svg
-                  class="w-3 h-3"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 14"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+      {viewFile && (
+        <div className='overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full h-screen md:inset-0 bg-transparent backdrop-blur-sm'>
+          <div className='relative p-4 pt-2 w-full h-full'>
+            <div className='relative rounded-lg shadow w-full h-full'>
+              <div className='flex items-center p-4 md:p-5 border border-gray-200 rounded-lg dark:border-gray-600 w-full h-full'>
+                <button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    closeView();
+                  }}
+                  className='absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex justify-center items-center'>
+                  <svg
+                    className='w-3 h-3'
+                    aria-hidden='true'
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 14 14'>
+                    <path
+                      stroke='currentColor'
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      strokeWidth='2'
+                      d='m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6'
+                    />
+                  </svg>
+                  <span className='sr-only'>Close modal</span>
+                </button>
+                {viewFile && (
+                  <ViewFile
+                    classroomName={classroomName}
+                    classroomid={classroomid}
+                    fileid={file.fileid}
                   />
-                </svg>
-                <span class="sr-only">Close modal</span>
-              </button>
-              {viewFile && (
-                <ViewFile
-                  classroomName={classroomName}
-                  classroomid={classroomid}
-                  fileid={file.fileid}
-                />
-              )}
+                )}
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
