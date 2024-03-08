d0eac3bbdf0475e23278ddfe155576123e8c0e0f
Praharsh-T <ppraharsht@gmail.com>
2024-03-08 19:34:57 +0530

    mininav added

--- client/src/Components/ClassSideBarComponents/MiniNav.jsx
+++ client/src/Components/ClassSideBarComponents/MiniNav.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 function MiniNav() {
   return (
-    <div>MiniNav</div>
+    <div>
+   
+<nav class="bg-gray-50 dark:bg-gray-700">
+    <div class="max-w-screen-xl px-4 py-3 mx-auto">
+        <div class="flex items-center">
+            <ul class="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
+                <li>
+                    <a href="#" class="text-gray-900 dark:text-white hover:underline" aria-current="page">Chats</a>
+                </li>
+                <li>
+                    <a href="#" class="text-gray-900 dark:text-white hover:underline">People</a>
+                </li>
+                <li>
+                    <a href="#" class="text-gray-900 dark:text-white hover:underline">Files</a>
+                </li>
+                
+            </ul>
+        </div>
+    </div>
+</nav></div>
   )
 }
 
