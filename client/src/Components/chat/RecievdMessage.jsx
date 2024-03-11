ce040059e00bd35bffd763403e73afa66ee4e0be
PavanDNaik <pawannaik396@gmail.com>
2024-03-12 03:07:08 +0530

    recievd msg display

--- client/src/Components/chat/RecievdMessage.jsx
+++ client/src/Components/chat/RecievdMessage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function RecievdMessage() {
+function RecievdMessage({ msg }) {
   return (
     <div class="flex justify-start mb-4">
       <img
@@ -9,9 +9,7 @@ function RecievdMessage() {
         alt=""
       />
       <div class="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat at
-        praesentium, aut ullam delectus odio error sit rem. Architecto nulla
-        doloribus laborum illo rem enim dolor odio saepe, consequatur quas?
+        {msg.msgcontent}
       </div>
     </div>
   );
