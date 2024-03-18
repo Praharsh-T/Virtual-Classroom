08f12df990333aa49a66e269027dfadea6c52357
Praharsh-T <ppraharsht@gmail.com>
2024-03-18 23:13:05 +0530

    changes in responsiveness part2

--- client/src/Components/ClassSideBarComponents/MiniNav.jsx
+++ client/src/Components/ClassSideBarComponents/MiniNav.jsx
@@ -14,11 +14,11 @@ function MiniNav({ classroomName }) {
   const [selected, setSeelected] = useState("");
   return (
     <nav className='bg-white dark:bg-gray-700 shadow-md'>
-      <div className='max-w-screen-xl px-4 py-3 mx-auto'>
+      <div className='max-w-screen-xl md:max-w-screen-xl  px-4 py-3 mx-auto'>
         <h1 class='text-2xl md:text-3xl pl-2 my-2 border-l-4 mb-4 font-sans font-bold border-blue-500 dark:text-gray-200'>
           {classroomName}
         </h1>
-        <ul className='flex flex-row md:flex-row items-start md:items-start justify-center md:justify-start space-x-8 rtl:space-x-reverse text-sm font-medium'>
+        <ul className='flex flex-row md:flex-row items-start md:items-center justify-center md:justify-start space-x-8 rtl:space-x-reverse text-sm font-medium'>
           <li className='px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-md hover:border-blue-500 transition-colors duration-300 ease-in-out last:border-r-0'>
             <Link
               to='./chat'
